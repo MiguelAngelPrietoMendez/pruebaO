002e662712c94a9956cd5401d09e88ec7904a67d
user <user@192.168.1.158>
2015-06-17 15:44:42 -0500

    EDITE H1 MMM

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-6">
-                        <h1 class="text-primary">Onteal Help Desk MICK</h1>
+                        <h1 class="text-primary">Onteal Help Desk MICK333</h1>
                         <h3>A subtit</h3>
                         <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
                             ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis
